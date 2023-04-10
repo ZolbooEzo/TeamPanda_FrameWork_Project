0f584cc806688803be5260b234f879e75cd42a26
ZolbooEzo <114514427+ZolbooEzo@users.noreply.github.com>
2023-04-10 16:33:40 -0400

    xpath edited

--- src/test/java/testing/PerformanceTest.java
+++ src/test/java/testing/PerformanceTest.java
@@ -13,7 +13,7 @@ import util.RetryAnalyzerTest;
 @Listeners(ListernersTestNG.class)
 public class PerformanceTest extends CommonMethods {
 
-	@Test(enabled = false, groups = { "all", "performance", "performance1" }, retryAnalyzer = RetryAnalyzerTest.class)
+	@Test(enabled = true, groups = { "all", "performance", "performance1" }, retryAnalyzer = RetryAnalyzerTest.class)
 	public void verifyThePerformanceMenuIsWorking() {
 		doLogin();
 		Assert.assertEquals(BaseClass.getDriver().getCurrentUrl(), Config.getRequiredData("dashboardUrl"));
@@ -29,7 +29,7 @@ public class PerformanceTest extends CommonMethods {
 		Assert.assertTrue(perp.noRecords.isDisplayed());
 	}
 
-	@Test(enabled = false, groups = { "all", "performance", "performance2" }, retryAnalyzer = RetryAnalyzerTest.class)
+	@Test(enabled = true, groups = { "all", "performance", "performance2" }, retryAnalyzer = RetryAnalyzerTest.class)
 	public void toVerifyTheSearchingOptionByNameIsWorking() {
 		doLogin();
 		Assert.assertEquals(BaseClass.getDriver().getCurrentUrl(), Config.getRequiredData("dashboardUrl"));
@@ -43,7 +43,7 @@ public class PerformanceTest extends CommonMethods {
 		Assert.assertEquals(perp.charlieText.getText(), "Charlie Carter");
 	}
 
-	@Test(enabled = true, groups = { "all", "performance", "performance3" })
+	@Test(enabled = true, groups = { "all", "performance", "performance3" }, retryAnalyzer = RetryAnalyzerTest.class)
 	public void toVerifyTheAddTrackersLogIsWorking() {
 		doLogin();
 		Assert.assertEquals(BaseClass.getDriver().getCurrentUrl(), Config.getRequiredData("dashboardUrl"));
